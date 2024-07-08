4ee676d6565fdc6ec1553f621feef8ed8cb26d89
Hottensiah Wanjiku <w.hottensiah96@gmail.com>
2024-07-08 14:00:23 +0300

    First commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.letters = sorted([letter for letter in word])
+
+    def match(self, word_list):
+        match_word_items = []
+
+        for word in word_list:
+            if sorted([letter for letter in word]) == self.letters:
+                match_word_items.append(word)
+
+        return match_word_items
+
+
